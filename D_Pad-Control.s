67ec0ceb7fe4026239ed5a7520e40e44abda8770
Pablo Elizondo Espinoza <147214546+pabloelizondo17@users.noreply.github.com>
2025-04-03 20:29:51 -0600

    Update D_Pad-Control.s

--- D_Pad-Control.s
+++ D_Pad-Control.s
@@ -20,11 +20,35 @@ D_PAD_RIGHT: .word 0xf000000c  # RIGHT button address
     _start:
     loop:
         # UP button reading:
+            li t0, 0xf0000000
+            lw t1, 0(t0)
+            bnez t1, move_up1
             
         # DOWN button reading:
+            li t0, 0xf0000000
+            lw t1, 4(t0)
+            bnez t1, move_down2
             
         # LEFT button reading:
+            li t0, 0xf0000000
+            lw t1, 8(t0)
+            bnez t1, move_down1
             
         # RIGHT button reading:
+            li t0, 0xf0000000
+            lw t1, 12(t0)
+            bnez t1, move_up2
+            
+    j loop
+    
+    move_up1:
+    j loop
         
-
+    move_up2:
+    j loop
+            
+    move_down1:
+    j loop
+                
+    move_down2:  
+    j loop
