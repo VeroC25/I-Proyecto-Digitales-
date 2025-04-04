ce4fc9da0f855fed686392b18269cdbcade462b8
Pablo Elizondo Espinoza <147214546+pabloelizondo17@users.noreply.github.com>
2025-04-04 11:39:57 -0600

    Update D_Pad-Control.s
    Actualización de direcciones. Se agrega matriz de LEDs

--- D_Pad-Control.s
+++ D_Pad-Control.s
@@ -7,12 +7,15 @@
 #-----------------------------------------------------
 
     .data
+LED_MATRIX_BASE: .word 0xf0000000
+MATRIX_WIDTH:    .word 0x37    # Matrix width
+MATRIX_HEIGHT:   .word 0x37    # Matrix height
     
-D_PAD_BASE:  .word 0xf0000000
-D_PAD_UP:    .word 0xf0000000  # UP button address
-D_PAD_DOWN:  .word 0xf0000004  # DOWN button address
-D_PAD_LEFT:  .word 0xf0000008  # LEFT button address
-D_PAD_RIGHT: .word 0xf000000c  # RIGHT button address
+D_PAD_BASE:  .word 0xf0002f44
+D_PAD_UP:    .word 0xf0002f44  # UP button address
+D_PAD_DOWN:  .word 0xf0002f48  # DOWN button address
+D_PAD_LEFT:  .word 0xf0002f4c  # LEFT button address
+D_PAD_RIGHT: .word 0xf0002f50  # RIGHT button address
 
     .text
     .globl _start
@@ -20,22 +23,22 @@ D_PAD_RIGHT: .word 0xf000000c  # RIGHT button address
     _start:
     loop:
         # UP button reading:
-            li t0, 0xf0000000
+            li t0, 0xf0002f44
             lw t1, 0(t0)
             bnez t1, move_up1
             
         # DOWN button reading:
-            li t0, 0xf0000000
+            li t0, 0xf0002f44
             lw t1, 4(t0)
             bnez t1, move_down2
             
         # LEFT button reading:
-            li t0, 0xf0000000
+            li t0, 0xf0002f44
             lw t1, 8(t0)
             bnez t1, move_down1
             
         # RIGHT button reading:
-            li t0, 0xf0000000
+            li t0, 0xf0002f44
             lw t1, 12(t0)
             bnez t1, move_up2
             
